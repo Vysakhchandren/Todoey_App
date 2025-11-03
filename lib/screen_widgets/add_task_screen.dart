9748581776b2ead49f146541b6690d60dad3982d
Vysakhchandren <vysakhc14@gmail.com>
2025-11-03 23:40:00 +0530

    Todoey first commit

---
+++ lib/screen_widgets/add_task_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  final Function addTaskCallback;
+
+  AddTaskScreen(this.addTaskCallback);
+
+  @override
+  Widget build(BuildContext context) {
+    String? newTaskTitle;
+    return Container(
+      padding: EdgeInsets.all(40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Add Task',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.lightBlueAccent,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          TextField(
+            autofocus: true,
+            textAlign: TextAlign.center,
+            onChanged: (newText) {
+              newTaskTitle = newText;
+            },
+            // decoration: InputDecoration(),
+          ),
+          SizedBox(height: 10),
+          TextButton(
+            child: Text('Add', style: TextStyle(color: Colors.white)),
+            style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.lightBlueAccent)
+            ),
+            onPressed: () {
+              addTaskCallback(newTaskTitle);
+            },
+          ),
+        ],
+      ),
+      //child: Text('Add Task'),
+    );
+  }
+}
---
+++ lib/screen_widgets/task_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:todoey_app/screen_widgets/add_task_screen.dart';
+
+import '../models/task.dart';
+import '../widgets/task_list.dart';
+import 'add_task_screen.dart';
+
+class TaskScreen extends StatefulWidget {
+  @override
+  State<TaskScreen> createState() => _TaskScreenState();
+}
+
+class _TaskScreenState extends State<TaskScreen> {
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) => AddTaskScreen((newTaskTitle) {
+              setState(() {
+                tasks.add(Task(name: newTaskTitle));
+              });
+              Navigator.pop(context);
+            }),
+          );
+        },
+        shape: CircleBorder(),
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    color: Colors.lightBlueAccent,
+                    size: 50,
+                  ),
+                  radius: 30,
+                  backgroundColor: Colors.white,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '${tasks.length} Tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                color: Colors.white,
+              ),
+              child: TasksList(tasks),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
